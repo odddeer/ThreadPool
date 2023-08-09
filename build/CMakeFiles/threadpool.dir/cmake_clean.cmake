ad1ff3596ec1dc324ddc9d5a850e6e70e0849460
Codedeer <codedeer@163.com>
2023-08-09 20:48:42 +0800

    first upload

---
+++ build/CMakeFiles/threadpool.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/threadpool.dir/test.cpp.o"
+  "CMakeFiles/threadpool.dir/threadpool.cpp.o"
+  "../bin/threadpool.pdb"
+  "../bin/threadpool"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/threadpool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
